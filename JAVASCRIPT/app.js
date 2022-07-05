1a3eb589dd5d9a705077d1064aa34f6f679e86bb
KINGKULLA <104991402+KINGKULLA@users.noreply.github.com>
2022-07-05 03:43:40 -0700

    Add files via upload

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,662 @@
+// document.body.style.backgroundColor='red';
+//   document.querySelectorAll('.btn').forEach((item) => {
+//    item.addEventlistener('click',() => {
+//     alert('Helllo World')
+//    })
+//   })
+
+// let firstname = 'Rotimi';
+// console.log(firstname);
+// const age =28;
+// console.log(age);
+// var country ='Nigeria'
+// console.log(country);
+
+
+
+// let animal = 'hippopotamus'; 
+// console.log(animal);
+// let lengthOfAnimal = animal.length;
+// console.log(lengthOfAnimal)
+
+
+// let Language = 'Yoruba'
+// console.log(language);
+// let positionOfLanguage=language[2]
+// console.log(positionOfLanguage);
+
+// // string methods
+
+// let surName ='austin'
+// console.log(surName)
+// let upperSurname =surName.toUpperCase()
+// console.log(upperSurname);
+
+// wrk
+
+// let x = 'Country'
+// console.log(x);
+
+// let y = 'State'
+// console.log(y);
+
+// let z = 'Country'
+// console.log(z)
+
+
+
+// let color2 = 'VIOLET';
+// console.log(color2);
+// let lowercolor =color2.toLowerCase()
+// console.log(lowerColor);
+
+// // indexOf
+// let email ='askjeevess@yahoo.com';
+// console.log(email);
+
+// let indexOfEmail = email.indexOf('a');
+// console.log(IndexOfEmail);
+
+// // replace()
+// let replace_email = email.replace('a', 'l');
+// console.log(replace_email);
+
+
+
+// String methods
+// upperCase()
+// let surName = 'austin';
+// console.log(surName);
+// let upperSurname = surName.toUpperCase()
+// console.log(upperSurname);
+// // lowercase()
+// let color2 = 'VIOLET';
+// console.log(color2);
+// let lowerColor = color2.toLowerCase()
+// console.log(lowerColor);
+// // indexOf
+// let email = 'askjeeves@yahoo.com';
+// console.log(email);
+// let indexOfEmail = email.indexOf('a');
+// console.log(indexOfEmail);
+// // replace()
+// let replace_email = email.replace('a', 'l');
+// console.log(replace_email);
+
+// // joining (concaterination) variables with strings
+// let first_name ='Sherifaat';
+// let newLastName ='Abiodun';
+// // let nomenclature =newFirstName + newLastName
+// let nomenclature =newFirstName +' '+ newLastName
+
+// console.log(nomenclature);
+
+ 
+
+// // number data type
+// let Year = 2022;
+// console.log(Year);
+// let score = 100
+// console.log(score);
+// let score = 200;
+// console.log(score2);
+// let num =10;
+// console.log(num);
+// let num2 = 3;
+// console.log(num2);
+
+// // math operators +,-,*./,**, %
+
+// let final_score = score + score 
+// console.log(final_score);
+
+// let minus = score2 - score
+// console.log(minus);
+
+// let mult = score * score2
+// console.log(mult);
+
+// let div =score2 / score
+// console.log(div);
+
+// let indices = num ** num2
+// console.log(indices);
+
+// let modulus = num % num2
+// console.log(modulus);
+
+// let addedYear = Year + 1
+// Year *= 9
+// console.log(Year);
+
+
+
+
+
+// class work correction
+
+
+// // Q1
+// let ANIMAL = 'HIPPOPOTAMUS';
+// let pick = ANIMAL[4].toLowerCase()
+// let upperAnimal = ANIMAL.toUpperCase()
+// console.log(upperAnimal);
+// // Q2
+// let lowerAnimal = ANIMAL.toLowerCase()
+// console.log(lowerAnimal);
+// // Q3
+// let positionOfT = ANIMAL.indexOf('T')
+// console.log(positionOfT);
+// // Q4
+// let indexOfLastO = ANIMAL.lastIndexOf('O')
+// console.log(indexOfLastO)
+
+// // Q5
+// let slice = ANIMAL.slice(5,8)
+// console.log(slice);
+// // Q6
+// let replace_h = ANIMAL.replace('H', 'B')
+// console.log(replace_h);
+
+
+ 
+
+// //  
+
+// let continent = 'AFRICA';
+// console.log(continent);
+// let language = 'PIDGIN';
+// console.log(language);
+// let GhanaPopulation = '500';
+// console.log(GhanaPopulation);
+// let FinlandPopulation = '800';
+// console.log(FinlandPopulation);
+// let country = 'NIGERIA';
+// console.log(country);
+// let NigerianPopulation = '1000';
+// console.log(NigerianPopulation);
+
+
+// Q1
+
+
+
+// let continent ='Africa';
+// console.log(continent);
+// let Language ='pidgin english';
+// console.log(Language);
+// let ghanaPopulation=500;
+// console.log(ghanaPopulation);
+// let finlandPopulation =800;
+// console.log( finlandPopulation);
+// let Country ='Nigeria';
+// console.log(Country);
+// let nigeriaPopulation=1000;
+// console.log(nigeriaPopulation);
+// // task 1
+// nigeriaPopulation/=2;
+// console.log(nigeriaPopulation);
+// // task 2
+// nigeriaPopulation++;
+// console.log(nigeriaPopulation);
+// // task 3
+// console.log('nigeriaPopulation'>'finlandPopulation');
+// // task 4
+// console.log('nigeriaPopulation'>'ghanaPopulation');
+// // task 5
+// let description = Country+ ' is in '+ continent +' and its '+ nigeriaPopulation +' people speak '+ Language;
+// console.log(description);
+
+
+
+
+
+// Template litrals or strings
+// let Description = `${Country} is in ${continent} and it's ${nigeriaPopulation} people speaks ${Language}`;
+// console.log(Description);
+// let Score = 90;
+// let TraineeName = 'Shrifaat';
+// let school = 'Aguda Grammar School';
+
+// let response = 'Hey Valentine ' + TraineeName + ' attends ' + school + ' and had ' + Score + ' in Her test';
+// console.log(response);
+
+// // Arrays
+// let favfood = 'banga'
+// let favfood2 = 'startch'
+// //  instead
+
+// let AustinFavFood = ['banga', 'startch', 'pounded-yam', 'ofesala'];
+// console.log(AustinFavFood)
+
+// // Array Properties
+
+// console.log(AustinFavFood.length);
+// AustinFavFood[0] ='Ewedu';
+// console.log(AustinFavFood);
+
+// // check console ewedu switches banga
+
+
+// // Array methods
+// let includes = AustinFavFood.includes('Amala')
+// console.log(includes);
+
+// // join
+// let join = AustinFavFood.join('_')
+// console.log(join);
+
+
+//  // indexOf()
+// let indexOf = AustinFavFood.indexOf('banga')
+// console.log(indexOf)
+// // push()
+// let push = AustinFavFood.push('Rice');
+// console.log(push);
+// console.log(AustinFavFood) 
+// // pop()
+// let pop = AustinFavFood.pop()
+// console.log(pop);
+// console.log(AustinFavFood)
+
+// let anodaPop = AustinFavFood.pop()
+// console.log(AustinFavFood)
+
+
+// // typof
+// let add = 1000;
+// console.log(typeof add);
+
+// let food = 'Hamburger';
+// console.log(typeof food);
+
+// let town 
+// console.log(typeof town);
+
+// let sth = null
+
+// console.log(typeof sth);
+
+// // spread operator
+// let foods = ['Garri', 'Beans', 'pap', 'spaghetti'];
+// console.log(foods);
+// let newFoods = ['eba', 'indomie', ...foods];
+// console.log(newFoods)
+// let numb = [1,2,3,4,5,6,7,8]
+// console.log(...numb);
+// console.log(numb);
+
+// // Array Destructure
+
+// let Colours = ['pink', 'yellow', 'blue', "white", 'red', 'grey']
+
+// // console.log(Colours.length);
+// // Colours[0] = 'violet';
+// // console.log(Colours);
+// // console.log(typeof Colours);
+
+// let [a,b,,...rest] = Colours;
+// console.log(a);
+// console.log(b);
+// console.log(...rest);
+
+
+// // Type conversion and coercion
+
+// // type conversion
+// const INPUTYEAR = '1991';
+// console.log(INPUTYEAR)
+// console.log(typeof INPUTYEAR)
+// console.log(INPUTYEAR  + 18)
+// console.log(Number(INPUTYEAR) + 18)
+// // lets try to convert sth dts not a number 
+// console.log('Eggy');
+// console.log(Number('Eggy'));
+// console.log(typeof NaN);
+// console.log(String(23), 23);
+
+// // type coercion(does d conversion behind the scene)
+// console.log('I am ' + 20 + ' Years old');
+// console.log('I am ' + String(20) + ' Years old');
+
+// console.log('23' - '10' - 3);
+// console.log('23' + year + 3 );
+
+// // 2320223 - Austin
+
+// console.log('23' * '2');
+// console.log('23' > '2');
+
+// let z = Number('1') + 1
+// z = z - 1
+// console.log(z);
+
+// console.log('10' - '4' - '3' - 2 + '5');
+// // Valentine , Erhime 15
+
+// // comparison operator
+// // >,<,==,===,!=, >=, <=
+// let Year1 = 2023;
+// console.log(Year1 > 2001);
+// console.log(Year1 < 2001);
+// console.log(Year1 >= 2001);
+// console.log(Year1 <= 2001);
+// console.log(Year1 != 2001);
+
+// // loose or abstarct comparison operator ==
+
+// let _score = '100';
+// let _score2 = _score == 100;
+// let _score3 = _score != 100;
+// console.log(_score2);
+// console.log(_score3);
+
+// // strict comparision operator ===
+
+// let _score4 = _score === 100
+// let _score5 = _score !== 100
+// console.log(_score4);
+// console.log(_score5);58  
+
+
+// let Traineename = ['austin','bewaji','emmanuel','erhime','sherifat','oluwatise','valentine','tope','rotimi','kingsley']
+// // Q1
+// Traineename[0]='pappi'
+// console.log(Traineename);
+// // Q2
+// Traineename.push('amos','mike')
+// console.log(Traineename);
+// // Q3
+// Traineename.pop('mike')
+// console.log(Traineename);
+// // Q4
+// Traineename.unshift('zack','bobby')
+// console.log(Traineename);
+// // Q5
+// Traineename.shift('zack')
+// console.log(Traineename);
+// // Q6
+// let newTraineename =['queen','segun','pius','emeka']
+// let alltraineename = Traineename.concat(newTraineename)
+// console.log(alltraineename);
+
+
+
+
+
+// 2 july 2022
+// ctrl flow has loops(for, while,and others) and conditionl statement(if,else if,else, switch)
+
+// loops
+// for - loop
+// for (let i = 0; i < 11; i++) {
+//   console.log(i)
+// }
+
+// let guys = ['Austin', 'Erhime', 'Valentine', 'Dami']
+
+// for (let g = 0; g < guys.length; g++) {
+//   console.log(guys[g])
+// }
+
+// // while loop
+// let babes = ['sherifat', 'latifat', 'lantana', 'Joy']
+
+// let r = 2
+
+// while (r < babes.length) {
+//   console.log(babes[r].toUpperCase())
+//   r++
+// }
+
+// // Conditional Statement(if,else if,else, switch)
+// // if
+// let AGE = 50
+
+// if (AGE === 50) {
+//   console.log('eligible')
+// }
+// // else
+// if (AGE === 5) {
+//   console.log('eligible')
+// } else {
+//   console.log('incorrect')
+// }
+// // else if
+// let PASSWORD = 'pass11111111'
+// // let PASSWORD = prompt('Type A password')
+
+// if (PASSWORD.length >= 12) {
+//   // alert('thats a very strong password')
+//     console.log('thats a very strong password')
+// } else if (PASSWORD.length >= 8) {
+//   // alert('password is good')
+
+//     console.log('password is good')
+// } else if (PASSWORD.length >= 4) {
+//   // alert('password is manageable')
+
+//     console.log('password is manageable')
+// } else {
+//   // alert(
+//   //   'Baba password not good enough,password lenght shuld be greater than or equal 8 characters'
+//   // )
+//     console.log(
+//       'Baba password not good enough,password lenght shuld be greater than or equal 8 characters'
+//     )
+// }
+
+
+
+
+
+// logical operators
+// || means OR
+// && means AND
+
+// True && True = True
+// True && False = False
+// False && True = False
+// False && False = False
+
+// True || True = True
+// True || False = True
+// False || True = True
+// False || False = False
+// ||-True
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// // &&-And
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+
+// let _PASSWORD = 'pass1@';
+
+// if(_PASSWORD.length >=7 && _PASSWORD.includes('@')){
+//     console.log('thats a strong password');
+// }else if(_PASSWORD.length >= 7 || _PASSWORD.includes('@')){
+//     console.log('password is good');
+
+// }else{
+//     console.log('Not good enuf');
+// }
+
+// const N = prompt('Enter A Number')
+
+// if(N % 2 !==0){
+//     alert('The number is odd')
+// }else{
+//     alert('The number is even')
+// }
+
+// Break And Continue
+// let _RESULT = [20,40,0,50,100,15,10]
+
+// for(let i = 0; i < _RESULT.length; i++){
+//     // What if we dnt wnt to log to awa console 0?
+//     if(_RESULT[i] === 0){
+//         continue
+//     }
+//     console.log(_RESULT[i]);
+//     if(_RESULT[i]===100){
+//         break
+//     }
+// }
+
+
+// let GRADE = 'Z'
+
+// if(GRADE === 'A'){
+//     console.log('You got A');
+// }else if(GRADE === 'B'){
+//     console.log('You got B');
+// }else if(GRADE === 'C'){
+//     console.log('You got C');
+// }else if(GRADE === 'D'){
+//     console.log('You got D');
+
+// }else{
+//     console.log('Photo,70 Years old man,nr b juju be dt');
+// }
+// // Switch Statement
+// let POINTS = 'Z'
+// switch(POINTS){
+//     case 'A':
+//         console.log('You got A');
+//         break
+//     case 'B':
+//         console.log('You got B');
+//         break
+//     case 'C':
+//         console.log('You got C');
+//         break
+//     case 'D':
+//         console.log('You got D');
+//         break
+//     default:
+//         console.log('Please repeat,points should be between A and D');
+// }
+
+// // Scope(Global,Block and function)
+
+// let $AGE = 100
+// if(true){
+//     let $AGE = 40
+//     console.log('Inside 1st block of code: ', $AGE);
+//     if(true){
+//         let $AGE = 300
+//         console.log('Inside 2nd block of code: ', $AGE);
+//     }
+// }
+
+// console.log('Outside block of code: ', $AGE);
+
+// // Functions(Declaration,expression and arrow function)
+// // Declaration
+// function team(){
+//     console.log('Valentine is now a senior dev');
+// }
+// // invoking,running or call the function
+// team()
+
+// // Expression
+// let Teams = function(){
+//     let speaker = 'Tope is around '
+//     console.log(speaker + 'today')
+// }
+
+// Teams()
+
+// // Parameters and arguments
+
+// function $case(name){
+//     console.log('Hello my name is ' + name);
+
+// }
+
+// $case('Dami')
+
+// // Arrow function
+
+// let _pick = session=>console.log(`Arrow function explained ${session}`)
+
+// _pick('today')
+
+// function express (name, time, place){
+//     let narrate = `Good ${time} ${name} ${place}`
+//     console.log(narrate)
+// }
+
+// express('Valentine', 'morning', 'where are you')
+
+
+
+// function CalculateBMI(Mass, Height){
+//   let BMI = (Mass/Height)** 2;}
+
+// let AustinBMI = (60, 10)
+// console.log(AustinBMI);
+
+// let SherifattBMI = (70, 9)
+// console.log(SherifattBMI);
+
+// if(AustinBMI > SherifattBMI ){
+//   console.log('Austin body mass index  is greater');
+// }
+// else if (SherifattBMI > AustinBMI){
+//   console.log('Sheriffat body mass index  is greater');
+// }
+// else{
+//     console.log('They are equal')
+
+
+// let BMI = (Mass/ Height)**2
+// let AustinBMI = (60, 10)
+// let SherifattBMI = (70, 9)
+// let CalcBMI = AustinBMI > SherifattBMI ?
+
+
+// 3rd JULY 2022
+
+
+[8:59 AM, 7/3/2022] +234 706 044 0986: let x = 5
+let y = 6
+let Z = x > y ? 'false':'true';
+console.log(Z);
+
+// Answers
+// Austins data
+let austinHeight = 10
+let austinMass= 60
+
+// Sherifats Data
+
+let sherifatHeight = 9
+let sherifatMass= 70
+
+
+// austins BMI
+let austinBMI = (austinMass / austinHeight) ** 2
+console.log(austinBMI)
+// sherifat BMI
+let sherifatBMI = (sherifatMass / sherifatHeight) ** 2
+console.log(sherifatBMI)
+
+// conditions
+if(austinBMI > sherifatBMI){
+    console.log('austin\'s BMI is greater than Sherifat\'s BMI ')
+}else{
+    console.log('Sherifat\'s BMI is greater than Autin\'s BMI');
+}
+
+// Using ternary operator
+let statement = austinBMI > sherifatBMI ? "Austin's BMI is greater than Sherifat's BMI":'Sherifat\'s BMI is greater than Austin\'s BMI'
+
+console.log(s…
+
